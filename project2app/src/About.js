da5b255d2124a0afdaf748a72918bdbfcda58310
Lauren Metz <Laurenmetz15@gmail.com>
2022-11-13 12:12:30 -0500

    add NavBar and About files, render about to page to check function

---
+++ project2app/src/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div id="about">
+            <h1>Memory Playlist</h1>
+            <h2>Why</h2>
+            <p>Studies have shown that music not only helps us retrieve memories but aids in making new ones. 
+               By creating memory playlists, you can unlock endless storage of the special memories that you
+               associate with a song. This website is designed to be used as a music journal, where you can record
+               your memories as an entry associated with a song. 
+            </p>
+            <h2>How To</h2>
+            <p>Using the naviagation bar at the top of the page you can click on the entry form to record a memory.
+               Input the details about the song of your choosing, the date, and a description of the memory associated with it, then 
+               submit the entry to save it to your memory playlist. In the Playlist tab you can then look through the song memories that 
+               you've added to your playlist in chronological order.
+            </p>
+
+        </div>
+    )
+}
+
+export default About;
---
+++ project2app/src/NavBar.js
@@ -0,0 +1,10 @@
+import { Route } from "react-router-dom";
+
+function NavBar() {
+
+    //switch with routes between playlist page, song form , about
+    //navbar should always be rendered 
+
+}
+
+export default NavBar;
