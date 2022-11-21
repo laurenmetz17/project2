d3ec3ce911bfd0a512ac19acfa57351556b3f3e2
Lauren Metz <Laurenmetz15@gmail.com>
2022-11-21 01:00:15 -0500

    style cards to make songtitle and artist uppercase

--- project2app/src/MemoryCard.js
+++ project2app/src/MemoryCard.js
@@ -5,14 +5,14 @@ function MemoryCard({entry, handleDelete}) {
     const {songTitle,artist,date, memory, albumCover, key ,audioPreview} = entry;
     console.log(key);
 
-    //incorporate audio preview
+    //incorporate audio preview and delete
 
     return(
         <div className="card">
             <img src={albumCover} alt="album cover"></img>
             <div className="container">
                 <h4>{`${date}`}</h4>
-                <h5>{`${songTitle}  - ${artist}`}</h5>
+                <h5>{`${songTitle.toUpperCase()}  - ${artist.toUpperCase()}`}</h5>
                 <p>{memory}</p>
                 {/*<a href={audioPreview}>ITunes Audio Preview</a>
                 <button id={key} onClick={handleDelete}>Delete Entry</button>*/}
